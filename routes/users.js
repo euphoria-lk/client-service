51476719a337a6f75d048b67bb3e4a68737ed8f7
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-05 13:30:40 +0530

    login endpoint and getuser by name added

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,53 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/login',async function(req, res, next){
+try{
+  const user =await User.findOne({
+    email:req.body.email,
+    password:req.body.password
+  })
+  if(!user){
+     res.status(200).send({
+      success: 'true',
+      message: "username or password incorrect"
+  })
+  }else{
+    res.status(201).send({
+      success: 'true',
+      message: "successful"
+    })
+  }
+
+}catch(err){
+  console.log(err)
+    res.status(500).send({message:err.message})
+}
+
+})
+router.post('/user',async function(req, res, next){
+try{
+  const user =await User.findOne({
+    firstname:req.body.firstname,
+    lastname:req.body.lastname
+  })
+  if(!user){
+     res.status(200).send({
+      success: 'true',
+      message: "User not found"
+  })
+  }else{
+    res.status(201).json(user);
+  }
+
+}catch(err){
+  console.log(err)
+    res.status(500).send({message:err.message})
+}
+
+})
+
+
 router.post('/signup', async function(req,res,next){
   try{
     const user= await User.findOne({
